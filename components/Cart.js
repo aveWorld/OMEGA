a03b54430be9a6885ea6ba2363b8a62e9543fc57
aveWorld <texnarb228@gmail.com>
2020-08-12 16:31:43 +0300

    fixed arrow

--- components/Cart.js
+++ components/Cart.js
@@ -10,7 +10,7 @@ const Cart = ({title, subtitle, img}) => (
        <div className="card__body">
         <h3 className="card__body__title">{title}</h3>
         <p className="card__body__subtitle">{subtitle}</p>
-        <div className="card__body__more bold-font"><img src="/arrow.svg" alt="arrow"/><span>Show More</span></div>
+        <div className="card__body__more bold-font"><img src="/Arrow.svg" alt="arrow"/><span>Show More</span></div>
        </div>
     </div>
 );
